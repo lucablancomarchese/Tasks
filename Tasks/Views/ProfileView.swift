bb9fa66c1504ce19ae6c702ac0250e5a45e6d2b0
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2024-02-05 16:23:30 +0100

    changed ProfileView UI

--- Tasks/Views/ProfileView.swift
+++ Tasks/Views/ProfileView.swift
@@ -13,7 +13,7 @@ struct ProfileView: View {
     
     var body: some View {
         NavigationView {
-            VStack (alignment: .leading){
+            ZStack {
                 if let user = viewModel.user {
                     profile(user: user)
                 } else {
@@ -27,27 +27,27 @@ struct ProfileView: View {
     
     @ViewBuilder
     func profile(user: User) -> some View {
-        Image(systemName: "person.circle")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 125, height: 125)
-        
-        HStack {
-            Text("First Name:").bold()
-            Text(user.firstname).padding()
+        VStack (alignment: .center, spacing: 20) {
+            Image(systemName: "person.circle")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 125, height: 125)
+                .padding(.top, 80)
+            
+            HStack {
+                Text("\(user.firstname) \(user.lastname)")
+                    .font(.system(.largeTitle))
+            }
+            
+            HStack {
+                Text("Joined:").bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))").padding()
+            }
+            Divider()
+            
+            TDButton(buttontitle: "Log Out", background: .red, action: viewModel.logOut)
+            Spacer()
         }
-        HStack {
-            Text("Last Name:").bold()
-            Text(user.lastname).padding()
-        }
-        HStack {
-            Text("Joined:").bold()
-            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))").padding()
-        }
-        
-        Button("Log Out") {viewModel.logOut()}.foregroundColor(.red).padding()
-        
-        Spacer()
     }
 }
 

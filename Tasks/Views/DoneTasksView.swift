0f5754f067a023d3dcc2e738478d07c5951bc511
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2024-02-05 19:03:57 +0100

    created View for Done Tasks

---
+++ Tasks/Views/DoneTasksView.swift
@@ -0,0 +1,34 @@
+//
+//  DoneTasksView.swift
+//  Tasks
+//
+//  Created by Luca Blanco Marchese on 05.02.24.
+//
+import FirebaseFirestoreSwift
+import SwiftUI
+
+struct DoneTasksView: View {
+    @StateObject var viewModel: ToDoListViewViewModel
+    @FirestoreQuery var items: [ToDoListItem]
+    
+    init(userId: String) {
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
+    }
+    var body: some View {
+        NavigationView {
+            VStack {
+                List(items) { item in
+                    if item.isDone {
+                        ToDoListItemView(item: item)
+                            .swipeActions { Button {viewModel.delete(id: item.id )} label: {Text("Delete")}}.tint(.red)
+                    }
+                }.listStyle(PlainListStyle())
+            }.navigationTitle("Done Tasks")
+        }
+    }
+}
+
+#Preview {
+    DoneTasksView(userId: "AzKT1TUszpUf9NK9BZmqM1ayfky2")
+}

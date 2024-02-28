f0e56d2776471911492b245416c59875ece33243
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2024-02-28 20:50:02 +0100

    added property "Type"

--- Tasks/Models/ToDoListItem.swift
+++ Tasks/Models/ToDoListItem.swift
@@ -10,6 +10,7 @@ import Foundation
 struct ToDoListItem: Codable, Identifiable {
     let id: String
     let title: String
+    let type: String
     let dueDate: TimeInterval
     let createdDate: TimeInterval
     var isDone: Bool
